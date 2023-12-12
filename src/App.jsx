1da2dcf5d4857dd1503eb998515adc19c3b27437
luckylab200903 <kumarkeshavsingh200903@gmail.com>
2023-12-12 11:53:21 +0530

    mern-estate project added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>Hello</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signout" element={<Signout/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Signout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Signout() {
+  return (
+    <div>
+      <h1>signout</h1>
+    </div>
+  )
+}
